ffdfbf23d4fc2b7a187ac32940b7d14510dee332
Nadezhda Chelyadinova <62635172+nadzyathedude@users.noreply.github.com>
2022-12-08 12:58:32 +0600

    day one - counting calories

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+//fun main(args: Array<String>) {
+//countCalories(elvesList)
+//
+//}
+//
+//
+////count how many calories elf has
+//
+//val elvesList = listOf(
+//    listOf(1, 2, 20),
+//    listOf(1, 30, 30,  30),
+//    listOf(1, 40),
+//    listOf(1, 50)
+//)
+//
+//fun countCalories (list : List<List<Int>>) =
+//    println(list.map { it.sum()}.max())
+
+
+
+
+
